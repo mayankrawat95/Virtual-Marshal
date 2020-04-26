0a2b24d121816beb43a74755186e39befb7d8e26
Mayank Rawat <mayankrawat95@gmail.com>
2020-04-26 19:03:33 +0530

    third push

--- js/initial.js
+++ js/initial.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     $('.slider').slick({
-        slidesToShow: 1,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
+
+$(document).ready(function(){
+    $('.card-holder').slick({
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
